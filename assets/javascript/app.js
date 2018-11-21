4dce615e08900340950deb3f4ef956fccb8a925b
z1995 <zhannaavanesova@yahoo.com>
2018-11-20 20:48:59 -0700

    Changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,19 @@
-$('select').on('change', function() {
-    var moodSelect = $('selecr option:selected').val();
+$('#dropdown a').on('click', function() {
+    var moodSelect = $(this).text().toLowerCase();
     var queryURL = 'https://api.spotify.com/v1/search?type=playlist&q=' + moodSelect + '&access_token=BQAFfu82ldJJKxXcvU2eRDb-2ImqcT-rhqGu2NZw36HUd-Kqpfy4BrYldgibgjWkm6nKYpoUqet6-QLhOGAgCSMiHBGyVDXd1VVc-BiS7heHZgOHiH_RFH6x2qxl041K9GQ8P_bpCiQRzZiYlnCZtT4v';
 
+    console.log("mood:" + moodSelect);
+    
     $.ajax({
         url: queryURL,
-        method: 'GET'
-    }).success(function(response){
+        method:'GET'
+    }).then(function(response){
         var embedCode = '<iframe src="https://open.spotify.com/embed/playlist/' + response.playlist + '" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
     $('#playerIframe').html(embedCode);
   })
     
 })
+
 
 // Initialize Firebase
 var config = {
@@ -23,3 +26,4 @@ var config = {
   };
   firebase.initializeApp(config);
   var database = firebase.database();
+
